1d8369a9f4815587bfce11ad9def0800f43e1b01
Nicolas Horton <nic.horton00@gmail.com>
2024-03-04 12:17:22 -0600

    Updated Link to use react router Link in copyright

--- src/components/copyright/Copyright.jsx
+++ src/components/copyright/Copyright.jsx
@@ -1,11 +1,12 @@
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import { Link as RLink } from 'react-router-dom';
 
 const Copyright = (props) => {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="/">
+      <Link component={RLink} color="inherit" to="/">
         ShelfSync
       </Link>{' '}
       {new Date().getFullYear()}
