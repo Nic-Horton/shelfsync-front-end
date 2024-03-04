e5a3fc24810e1de522b68af64471d5204113d3cb
Nicolas Horton <nic.horton00@gmail.com>
2024-03-04 12:13:12 -0600

    Created interface for sign in and sign up pages

--- src/components/signinPage/SignInPage.jsx
+++ src/components/signinPage/SignInPage.jsx
@@ -1,8 +1,75 @@
-
+import CssBaseline from '@mui/material/CssBaseline';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { Link as RLink } from 'react-router-dom';
+import Link from '@mui/material/Link';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Copyright from '../copyright/Copyright'
 
 const SignInPage = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      username: data.get('username'),
+      password: data.get('password'),
+    });
+  };
+
   return (
-    <div>SignInPage</div>
+    <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            paddingTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="current-username"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Box sx={{display:'flex', justifyContent:'center'}}>
+                <Link component={RLink} to="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+            </Box>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
   )
 }
 
--- src/components/signupPage/SignUpPage.jsx
+++ src/components/signupPage/SignUpPage.jsx
@@ -1,8 +1,88 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { Link as RLink } from 'react-router-dom';
+import Link from '@mui/material/Link';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Copyright from '../copyright/Copyright'
 
 
 const SignUpPage = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      username: data.get('username'),
+      password: data.get('password'),
+    });
+  };
+
   return (
-    <div>SignUpPage</div>
+    <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            paddingTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              type='email'
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoFocus
+              autoComplete="new-user-name"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="new-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Box sx={{display:'flex', justifyContent:'center'}}>
+                <Link component={RLink} to="/signin" variant="body2">
+                  {"Have an account already? Sign In"}
+                </Link>
+            </Box>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
   )
 }
 
