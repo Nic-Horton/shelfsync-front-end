26167c7a2226cafaa54398afe02c3e93bcbd27e8
Nicolas Horton <nic.horton00@gmail.com>
2024-03-17 10:57:30 -0500

    Added ability to sort data in asc and dsc order. Also added a display to show if no data is found on search or if data is empty on backend.

--- src/components/inventory/Inventory.jsx
+++ src/components/inventory/Inventory.jsx
@@ -15,13 +15,63 @@ import ItemModal from './ItemModal';
 import { getInventory } from '../../api/pantryItems';
 import NewItemModal from './NewItemModal';
 import SearchInput from './SearchInput';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import { visuallyHidden } from '@mui/utils';
 
+const headCells = [
+  {
+    id: 'name',
+    numeric: false,
+    label: 'Name',
+  },
+  {
+    id: 'quantity',
+    numeric: true,
+    label: 'Quantity',
+  },
+  {
+    id: 'unit',
+    numeric: false,
+    label: 'Unit',
+  },
+  {
+    id: 'category',
+    numeric: false,
+    label: 'Category',
+  },
+];
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+function getComparator(order, orderBy) {
+  return (a, b) => {
+    if (a[orderBy] < b[orderBy]) {
+      return order === 'asc' ? -1 : 1;
+    }
+    if (a[orderBy] > b[orderBy]) {
+      return order === 'asc' ? 1 : -1;
+    }
+    return 0;
+  };
+}
 
 const Inventory = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [open, setOpen] = useState(false); 
   const [openForAdd, setOpenForAdd] = useState(false); 
   const [selectedRow, setSelectedRow] = useState({}); 
+  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState('name');
 
   const handleOpen = (item) => {
     setSelectedRow(item)
@@ -50,11 +100,18 @@ const Inventory = () => {
     return <h1>Error: {inventoryQuery.error.message}</h1>
   }
 
-  const filteredItems = searchTerm ? inventoryQuery.data.filter(item =>
-    item.name.toLowerCase().includes(searchTerm.toLowerCase())
-    ||
+  const filteredItems = inventoryQuery.data.filter(item =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
     item.category.toLowerCase().includes(searchTerm.toLowerCase())
-  ) : inventoryQuery.data;
+  );
+
+  const sortedItems = stableSort(filteredItems, getComparator(order, orderBy));
+
+  const handleRequestSort = (property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
 
   return (
     <>
@@ -63,6 +120,8 @@ const Inventory = () => {
       sx={{
         pl: { sm: 2 },
         pr: { xs: 1, sm: 1 },
+        display:'flex',
+        justifyContent:'start'
       }}
     >
       <Typography
@@ -73,34 +132,52 @@ const Inventory = () => {
       >
         Pantry Inventory
       </Typography>
-      <Tooltip title="Create New Item">
-        <IconButton onClick={() => handleOpenForAdd()}>
+      <Tooltip title="Add Item">
+        <IconButton  onClick={() => handleOpenForAdd()}>
           <FaRegPlusSquare />
         </IconButton>
       </Tooltip>
     </Toolbar>
-    <Toolbar>
+    <Toolbar sx={{display:'flex', justifyContent:'center' }}>
       <SearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
     </Toolbar>
     <Table size="large">
       <TableHead>
         <TableRow>
-          <TableCell>Name</TableCell>
-          <TableCell>Quantity</TableCell>
-          <TableCell>Unit</TableCell>
-          <TableCell align="right">Category</TableCell>
+        {headCells.map((headCell) => (
+                <TableCell
+                  key={headCell.id}
+                  align={ 'left' }
+                  padding={ 'normal'}
+                >
+                  <TableSortLabel
+                    active={orderBy === headCell.id}
+                    direction={orderBy === headCell.id ? order : 'asc'}
+                    onClick={() => handleRequestSort(headCell.id)}
+                  >
+                    {headCell.label}
+                    {orderBy === headCell.id ? (
+                      <Box component="span" sx={visuallyHidden}>
+                        {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                      </Box>
+                    ) : null}
+                  </TableSortLabel>
+                </TableCell>
+              ))}
         </TableRow>
       </TableHead>
+      {sortedItems.length === 0 ? <Typography variant='h5'>No Items Found</Typography> :
       <TableBody>
-        {filteredItems?.map((item) => (
+        {sortedItems?.map((item) => (
           <TableRow hover sx={{ cursor: 'pointer' }} onClick={() => handleOpen(item)} key={item.id}>
-            <TableCell>{item.name}</TableCell>
-            <TableCell>{item.quantity}</TableCell>
-            <TableCell>{item.unit === 'null' ? '' : item.unit}</TableCell>
-            <TableCell align="right">{item.category}</TableCell>
+            <TableCell align="left">{item.name}</TableCell>
+            <TableCell align="left">{item.quantity}</TableCell>
+            <TableCell align="left">{item.unit === 'null' ? '' : item.unit}</TableCell>
+            <TableCell align="left">{item.category}</TableCell>
           </TableRow>
         ))}
       </TableBody>
+}
     </Table>
     </Box>
     <ItemModal open={open} handleClose={handleClose} selectedRow={selectedRow}/>
