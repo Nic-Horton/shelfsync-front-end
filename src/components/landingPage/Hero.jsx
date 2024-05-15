89344e7607a841f773894bc2568aca23c8a2ad84
Nicolas Horton <nic.horton00@gmail.com>
2024-05-15 14:15:50 -0500

    Changed weight of text in landing page

--- src/components/landingPage/Hero.jsx
+++ src/components/landingPage/Hero.jsx
@@ -1,42 +1,58 @@
-
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import HeroLayout from './HeroLayout';
 import { Link } from 'react-router-dom';
 import { isAuthenticated } from '../../../auth';
 
-
-const backgroundImage ='../src/assets/strawberries.jpg';
+const backgroundImage = '../src/assets/strawberries.jpg';
 
 export default function Hero() {
-  return (
-    <HeroLayout
-      sxBackground={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundColor: '#242424',
-        backgroundPosition: '50%',
-        backgroundSize: "cover",
-      }}
-    >
-      <img
-        style={{ display: 'none' }}
-        src={backgroundImage}
-        alt="increase priority"
-      />
-      <Typography color="inherit" align="center" variant="h2" marked="center">
-        Shelfsync
-      </Typography>
-      <Typography
-        color="inherit"
-        align="center"
-        variant="h5"
-        sx={{ mb: 4, mt: { xs: 4, sm: 10 } }}
-      > 
-      Where your pantry syncs its shelf.
-      </Typography>
-      <Button component={Link} to={isAuthenticated ? 'dashboard' : 'signin'} variant="contained" size="large" sx={{width: 'fit-content', alignSelf:'center', p:'10px'}}> 
-        Get Started
-      </Button>
-    </HeroLayout>
-  );
+	return (
+		<HeroLayout
+			sxBackground={{
+				backgroundImage: `url(${backgroundImage})`,
+				backgroundColor: '#242424',
+				backgroundPosition: '50%',
+				backgroundSize: 'cover',
+			}}
+		>
+			<img
+				style={{ display: 'none' }}
+				src={backgroundImage}
+				alt="increase priority"
+			/>
+			<Typography
+				color="inherit"
+				align="center"
+				variant="h1"
+				fontWeight="bold"
+				marked="center"
+			>
+				Shelfsync
+			</Typography>
+			<Typography
+				color="inherit"
+				align="center"
+				variant="h3"
+				fontWeight="bold"
+				sx={{ mb: 4, mt: { xs: 4, sm: 10 } }}
+			>
+				Where your pantry syncs its shelf.
+			</Typography>
+			<Button
+				component={Link}
+				to={isAuthenticated ? 'dashboard' : 'signin'}
+				variant="contained"
+				size="large"
+				sx={{
+					width: 'fit-content',
+					alignSelf: 'center',
+					p: '10px',
+					fontWeight: 'bold',
+				}}
+			>
+				Get Started
+			</Button>
+		</HeroLayout>
+	);
 }
